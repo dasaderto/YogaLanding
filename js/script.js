310a6334d6f849a23f694a8d5f94a137af2854b6
Nikita Esipov <38347951+dasaderto@users.noreply.github.com>
2019-04-14 14:03:12 +0300

    append Timer functions

--- js/script.js
+++ js/script.js
@@ -34,4 +34,45 @@ window.addEventListener('DOMContentLoaded', function(){
         }
     });
 
+    // Timer
+
+    let deadline = '2019-04-15';
+
+    function getTimeRemaining(endtime){
+        let t = Date.parse(endtime)-Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor(t/(1000*3600));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+            
+        let updateClock = () =>{
+            let t = getTimeRemaining(endtime);
+            (t.hours < 10) ? hours.textContent = "0" + t.hours : hours.textContent = t.hours;
+            (t.minutes < 10) ? minutes.textContent = "0" + t.minutes : minutes.textContent = t.minutes;
+            (t.seconds < 10) ? seconds.textContent = "0" + t.seconds : seconds.textContent = t.seconds;
+
+            if (t.total<=0) {
+                clearInterval(itv);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        };
+
+        let itv = setInterval(updateClock, 1000);
+    }
+    setClock("timer", deadline);
 });
