70a82532b00a52d329251fcdbd1c1a9fe7c35221
Nikita Esipov <38347951+dasaderto@users.noreply.github.com>
2019-04-17 00:59:22 +0300

    append server modal Form functions

--- js/script.js
+++ js/script.js
@@ -75,4 +75,63 @@ window.addEventListener('DOMContentLoaded', function(){
         let itv = setInterval(updateClock, 1000);
     }
     setClock("timer", deadline);
+
+    // modal window
+
+    let more = document.querySelector('.more');
+    let overlay = document.querySelector('.overlay');
+    let close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function(){
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click',function(){
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    // Forms
+
+    let message = {
+        loading : "Загрузка...",
+        success : "Спасибо за заявку, скоро мы с вами свяжемся",
+        failure : "Что-то пошло не так..."
+    };
+
+    let form = document.querySelector('.main-form');
+    let input = form.getElementsByTagName('input');
+    let statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        let formData = new FormData(form);
+
+        request.send(formData);
+
+        request.addEventListener('readystatechange', function(){
+            if(request.readyState < 4){
+                statusMessage.innerHTML = message.loading;
+            }else if(request.readyState == 4 && request.status == 200){
+                statusMessage.innerHTML = message.success;
+            }else{
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for(let i=0; i < input.length; i++){
+            input[i].value = "";
+        }
+    });
 });
